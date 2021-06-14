00005a2ec7f1e094ee1c4a0b6bbc3a86b17404cb
DkuzRepository <SpiritHamster123>
2021-06-14 15:46:10 +0300

    Фильтр Габора с комментариями

--- Filter GAbor.py
+++ Filter GAbor.py
@@ -3,44 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Grayscale
+
 def BGR2GRAY(img):
     # Grayscale
     gray = 0.2126 * img[..., 2] + 0.7152 * img[..., 1] + 0.0722 * img[..., 0]
     return gray
 
 
-# Gabor Filter
 def Gabor_filter(K_size=111, Sigma=10, Gamma=1.2, Lambda=10, Psi=0, angle=0):
     # get half size
     d = K_size // 2
 
 
 
-    # prepare kernel
+
     gabor = np.zeros((K_size, K_size), dtype=np.float32)
 
-    # each value
+
     for y in range(K_size):
         for x in range(K_size):
-            # distance from center
+
             px = x - d
             py = y - d
 
-            # degree -> radian
+
             theta = angle / 180. * np.pi
 
-            # get kernel x
+
             _x = np.cos(theta) * px + np.sin(theta) * py
 
-            # get kernel y
+
             _y = -np.sin(theta) * px + np.cos(theta) * py
 
-            # fill kernel
+
             gabor[y, x] = np.exp(-(_x ** 2 + Gamma ** 2 * _y ** 2) / (2 * Sigma ** 2)) * np.cos(
                 2 * np.pi * _x / Lambda + Psi)
 
-    # kernel normalization
     gabor /= np.sum(np.abs(gabor))
 
     return gabor
@@ -73,14 +71,13 @@ def Gabor_filtering(gray, K_size=111, Sigma=10, Gamma=1.2, Lambda=10, Psi=0, ang
 
 # Используйте 6 фильтров Габора с разными углами для извлечения деталей на изображении
 def Gabor_process(img):
-    # get shape
+
     H, W, _ = img.shape
 
     # gray scale
     gray = BGR2GRAY(img).astype(np.float32)
 
-    # define angle
-    # As = [0, 45, 90, 135]
+
     As = [0, 30, 60, 90, 120, 150]
 
     # prepare pyplot
@@ -88,15 +85,15 @@ def Gabor_process(img):
 
     out = np.zeros([H, W], dtype=np.float32)
 
-    # each angle
+
     for i, A in enumerate(As):
         # gabor filtering
         _out = Gabor_filtering(gray, K_size=11, Sigma=1.5, Gamma=1.2, Lambda=3, angle=A)
 
-        # add gabor filtered image
+
         out += _out
 
-    # scale normalization
+
     out = out / out.max() * 255
     out = out.astype(np.uint8)
 
